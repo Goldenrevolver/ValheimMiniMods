0cbb1dc2a1a459d0da8350b900c27209e9ec1037
Goldenrevolver <22944589+Goldenrevolver@users.noreply.github.com>
2022-12-22 22:19:10 +0100

    Mini mod: Instantly Destroy Boats And Carts

---
+++ InstantlyDestroyBoatsAndCarts/InstantlyDestroyBoatsAndCarts/BoatWagonPatches.cs
@@ -0,0 +1,87 @@
+﻿using HarmonyLib;
+using static InstantlyDestroyBoatsAndCarts.InstantlyDestroyBoatsAndCartsPlugin;
+
+namespace InstantlyDestroyBoatsAndCarts
+{
+    internal class BoatWagonPatches
+    {
+        [HarmonyPatch(typeof(Piece), nameof(Piece.Awake))]
+        public static class Awake_Patch
+        {
+            private static void Postfix(Piece __instance)
+            {
+                Ship ship = __instance.GetComponentInChildren<Ship>();
+                Vagon vagon = __instance.GetComponentInChildren<Vagon>();
+
+                if (ship || vagon)
+                {
+                    __instance.m_canBeRemoved = true;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(Piece), nameof(Piece.CanBeRemoved))]
+        public static class CanBeRemoved_Patch
+        {
+            private static bool Prefix(Piece __instance, ref bool __result)
+            {
+                Ship ship = __instance.GetComponentInChildren<Ship>();
+                Vagon cart = __instance.GetComponentInChildren<Vagon>();
+
+                if (!ship && !cart)
+                {
+                    return true;
+                }
+
+                __result = false;
+
+                var config = AllowDetroyFor.Value;
+
+                if (ship != null && (config == AllowDestroy.OnlyBoats || config == AllowDestroy.Both))
+                {
+                    Container container = __instance.GetComponentInChildren<Container>();
+
+                    if (!CanRemoveContainer(container))
+                    {
+                        return false;
+                    }
+
+                    // base game check from ship.CanBeRemoved(): check if players are on board
+                    __result = ship.m_players.Count == 0;
+                    return false;
+                }
+
+                if (cart != null && (config == AllowDestroy.OnlyCarts || config == AllowDestroy.Both))
+                {
+                    if (!CanRemoveContainer(cart.m_container))
+                    {
+                        return false;
+                    }
+
+                    __result = cart.InUse();
+                    return false;
+                }
+
+                return false;
+            }
+        }
+
+        // custom version of Container.CanRemove with a null check and config check
+        private static bool CanRemoveContainer(Container container)
+        {
+            if (!container)
+            {
+                return false;
+            }
+
+            var isPrivate = container.m_privacy == Container.PrivacySetting.Private;
+
+            if ((isPrivate || PreventWhenContainerIsNotEmpty.Value) && container.GetInventory().NrOfItems() > 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ InstantlyDestroyBoatsAndCarts/InstantlyDestroyBoatsAndCarts/InstantlyDestroyBoatsAndCartsPlugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System.Reflection;
+
+namespace InstantlyDestroyBoatsAndCarts
+{
+    [BepInPlugin("goldenrevolver.instantly_destroy_boats_and_carts", NAME, VERSION)]
+    public class InstantlyDestroyBoatsAndCartsPlugin : BaseUnityPlugin
+    {
+        public const string NAME = "Instantly Destroy Boats And Carts";
+        public const string VERSION = "1.0";
+
+        public static ConfigEntry<AllowDestroy> AllowDetroyFor;
+        public static ConfigEntry<bool> PreventWhenContainerIsNotEmpty;
+
+        protected void Awake()
+        {
+            LoadConfig();
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        private void LoadConfig()
+        {
+            AllowDetroyFor = Config.Bind("General", nameof(AllowDetroyFor), AllowDestroy.Both, $"Which of these you can now immediately destroy with a hammer, if {nameof(PreventWhenContainerIsNotEmpty)} and the internal checks allow it.");
+            PreventWhenContainerIsNotEmpty = Config.Bind("General", nameof(PreventWhenContainerIsNotEmpty), true, "Whether the container of the boat or wagon needs to be empty to allow for it to get immediately destroyed with a hammer.");
+        }
+
+        public enum AllowDestroy
+        {
+            Disabled,
+            OnlyBoats,
+            OnlyCarts,
+            Both
+        }
+    }
+}
