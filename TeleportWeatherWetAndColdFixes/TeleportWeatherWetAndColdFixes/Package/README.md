4e81f153854e7cb9cad89879dc364f9e4a640aac
Goldenrevolver <22944589+Goldenrevolver@users.noreply.github.com>
2022-12-26 18:59:03 +0100

    New mod and release: TeleportWeatherWetAndColdFixes

---
+++ TeleportWeatherWetAndColdFixes/TeleportWeatherWetAndColdFixes/Package/README.md
@@ -0,0 +1,12 @@
+This mod changes teleporting with a portal to instantly update the weather on arrival, and optionally removes wet, cold and freezing debuffs at the end of the teleport.
+
+This mod is a standalone version of the teleporting changes from my other new mod 'Cape and Torch Resistance Changes - Water and Cold Resistance" (so only install one of them).
+
+Changes:
+
+In the base game, the weather takes a few seconds to update after teleporting, because the transition period used for normal biome transition still gets applied. This mod changes that:
+- Teleporting with a portal now immediately updates the weather at the new location, so you don't get wet from rain that doesn't exist
+- Optionally removes wet, cold and freezing debuffs at the end of the teleport
+
+
+Source code available on github: https://github.com/Goldenrevolver/ValheimMiniMods
---
+++ TeleportWeatherWetAndColdFixes/TeleportWeatherWetAndColdFixes/TeleportWeatherWetAndColdFixesPlugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System.Reflection;
+
+//using static TeleportWeatherWetAndColdFixes.TeleportHelper;
+
+namespace TeleportWeatherWetAndColdFixes
+{
+    [BepInIncompatibility("goldenrevolver.CapeAndTorchResistanceChanges")]
+    [BepInPlugin("goldenrevolver.TeleportWeatherWetAndColdFixes", NAME, VERSION)]
+    public class TeleportWeatherWetAndColdFixesPlugin : BaseUnityPlugin
+    {
+        public const string NAME = "Teleport Instantly Updates Weather - Removes Wet Debuff";
+        public const string VERSION = "1.0";
+
+        public static ConfigEntry<TeleportChange> TeleportChange;
+
+        protected void Awake()
+        {
+            LoadConfig();
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        private void LoadConfig()
+        {
+            var sectionName = "General";
+
+            TeleportChange = Config.Bind(sectionName, nameof(TeleportChange), TeleportWeatherWetAndColdFixes.TeleportChange.InstantlyUpdateWeatherAndClearWetAndColdDebuff, string.Empty);
+        }
+    }
+}
