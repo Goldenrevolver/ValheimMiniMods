a20a92d4887735888a6b6f34b950c46cfee32ed9
Goldenrevolver <22944589+Goldenrevolver@users.noreply.github.com>
2023-10-17 16:39:55 +0200

    New mod and release: Log Cleanup

---
+++ LogCleanup/LogCleanup/LogCleanupPlugin.cs
@@ -0,0 +1,62 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Reflection;
+
+namespace LogCleanup
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class LogCleanupPlugin : BaseUnityPlugin
+    {
+        public const string GUID = "goldenrevolver.LogCleanup";
+        public const string NAME = "Log Cleanup";
+        public const string VERSION = "1.0.0";
+
+        protected void Awake()
+        {
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch]
+    internal static class LoggingPatches
+    {
+        private static bool doneSuppressing = false;
+
+        [HarmonyPatch(typeof(FejdStartup), nameof(FejdStartup.Start)), HarmonyPrefix, HarmonyPriority(Priority.First - 1)]
+        public static void DisableSuppression()
+        {
+            doneSuppressing = true;
+        }
+
+        [HarmonyPatch(typeof(ZLog), nameof(ZLog.LogWarning)), HarmonyPrefix]
+        public static bool SuppressBaseGameWarning(object o)
+        {
+            return doneSuppressing || !(o is string s && IsUnwanted(s));
+        }
+
+        private static bool IsUnwanted(string s)
+        {
+            switch (s)
+            {
+                case "Fetching PlatformPrefs 'GuiScale' before loading defaults":
+                case "Set button \"CamZoomIn\" to None!":
+                case "Set button \"CamZoomOut\" to None!":
+                case "Missing audio clip in music respawn":
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+
+        private const string fuckYou = "Only custom filters can be played. Please add a custom filter or an audioclip to the audiosource (Amb_MainMenu).";
+
+        [HarmonyPatch(typeof(BepInEx.Logging.Logger), "InternalLogEvent", new Type[] { typeof(object), typeof(LogEventArgs) }), HarmonyPrefix]
+        public static bool SuppressTheSpecialLogDirectlyInBepInEx(object sender, LogEventArgs eventArgs)
+        {
+            return doneSuppressing || !(eventArgs.Data is string s && s == fuckYou);
+        }
+    }
+}
---
+++ LogCleanup/LogCleanup/Package/README.md
@@ -0,0 +1,9 @@
+This mod removes the 8 yellow warnings you get before the main menu. Not a single warning more or less, exactly those specific 8, that's it.
+
+These are base game issues that IronGate is ignoring due to how irrelevant they are.
+
+This mod does not affect warnings from mods and it turns itself off once you reach the main menu. Enjoy!
+
+![image](https://staticdelivery.nexusmods.com/mods/3667/images/2567/2567-1697545807-380815206.gif)
+
+Source code available on github: https://github.com/Goldenrevolver/ValheimMiniMods
