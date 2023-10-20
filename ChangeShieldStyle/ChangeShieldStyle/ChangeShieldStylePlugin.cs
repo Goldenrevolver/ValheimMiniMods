93d999133f3359075e2668a651fd0f7e77927d8e
Goldenrevolver <22944589+Goldenrevolver@users.noreply.github.com>
2023-10-20 06:33:14 +0200

    New mod and release: Change shield style

---
+++ ChangeShieldStyle/ChangeShieldStyle/ChangeShieldStylePlugin.cs
@@ -0,0 +1,72 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+
+namespace ChangeShieldStyle
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class ChangeShieldStylePlugin : BaseUnityPlugin
+    {
+        public const string GUID = "goldenrevolver.ChangeShieldStyle";
+        public const string NAME = "Change Style Of Existing Shield";
+        public const string VERSION = "1.0.0";
+
+        protected void Awake()
+        {
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch]
+    internal static class Patches
+    {
+        [HarmonyPatch(typeof(CraftingStation), nameof(CraftingStation.UseItem)), HarmonyPrefix]
+        public static bool UpdateShieldStylePatch(CraftingStation __instance, Humanoid user, ItemDrop.ItemData item, ref bool __result)
+        {
+            var stationName = Utils.GetPrefabName(__instance.gameObject);
+
+            if (stationName != "piece_workbench" && stationName != "forge")
+            {
+                return true;
+            }
+
+            return !TryUpdateShieldStyle(user, item, ref __result);
+        }
+
+        public static bool TryUpdateShieldStyle(Humanoid user, ItemDrop.ItemData item, ref bool __result)
+        {
+            if (!user.IsPlayer() || user != Player.m_localPlayer || !user.TakeInput())
+            {
+                return false;
+            }
+
+            if (item.m_shared.m_itemType != ItemDrop.ItemData.ItemType.Shield)
+            {
+                return false;
+            }
+
+            if (item.m_shared.m_icons == null || item.m_shared.m_icons.Length == 0)
+            {
+                return false;
+            }
+
+            if (item.m_variant < 0 || item.m_variant >= item.m_shared.m_icons.Length)
+            {
+                return false;
+            }
+
+            item.m_variant = (item.m_variant + 1) % item.m_shared.m_icons.Length;
+
+            // in the base game, this is always true for a shield, but you never know if a mod patches it
+            if (item.IsEquipable())
+            {
+                // updates the visual equipment, otherwise you need to unequip and reequip it yourself
+                user.UnequipItem(item);
+                user.EquipItem(item);
+            }
+
+            __result = true;
+            return true;
+        }
+    }
+}
---
+++ ChangeShieldStyle/ChangeShieldStyle/Package/README.md
@@ -0,0 +1,12 @@
+This mod allows you to change the style of a shield at any point after crafting it.
+
+Simply walk up to a workbench or forge and use your shield on it the same way you offer items to a boss altar.
+
+It will cycle through all possible options. Enjoy changing your fashion!
+
+![image](https://staticdelivery.nexusmods.com/mods/3667/images/2575/2575-1697774132-1873733619.gif)
+
+![image](https://staticdelivery.nexusmods.com/mods/3667/images/2575/2575-1697774144-299622244.gif)
+\
+\
+Source code available on github: https://github.com/Goldenrevolver/ValheimMiniMods
